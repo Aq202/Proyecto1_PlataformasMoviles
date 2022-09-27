2443f0346a084e9b6a3cf7b1764fc9868edbf73b
Diego <diegoguatedb2002@gmail.com>
2022-09-27 13:22:11 -0600

    Navigation, toolbar, bottomNavBar y login Fragment agregados

---
+++ Proyect_app/app/src/main/java/com/example/proyecto_final_apps/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.proyecto_final_apps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.isVisible
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: MaterialToolbar
+    private lateinit var bottomNavBar: BottomNavigationView
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbar)
+        bottomNavBar = findViewById(R.id.bottomNavigationBar)
+
+        //configuar toolbar
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        val appbarConfig = AppBarConfiguration(navController.graph)
+        toolbar.setupWithNavController(navController, appbarConfig)
+
+        listenToNavGraphChanges()
+    }
+
+    private fun listenToNavGraphChanges(){
+
+        //Detectar cambios en la navegación
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+
+            //mostrar y ocultar el toolbar y bottomNavBar
+            if(destination.id in setOf(R.id.loginFragment)){
+                toolbar.isVisible = false
+                bottomNavBar.isVisible = false
+            }else{
+                toolbar.isVisible = true
+                bottomNavBar.isVisible = true
+            }
+
+        }
+
+    }
+}
