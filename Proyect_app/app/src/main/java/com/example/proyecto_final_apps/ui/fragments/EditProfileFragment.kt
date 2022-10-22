bb98f758835ab2e7a044d496023cfabb87551f90
pabloozamora <pabloozamora02@gmail.com>
2022-10-21 22:57:04 -0600

    EditProfile layout finalizado y Signup layout corregido

--- Proyect_app/app/src/main/java/com/example/proyecto_final_apps/ui/fragments/EditProfileFragment.kt
+++ Proyect_app/app/src/main/java/com/example/proyecto_final_apps/ui/fragments/EditProfileFragment.kt
@@ -6,9 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.proyecto_final_apps.R
+import com.example.proyecto_final_apps.databinding.FragmentEditProfileBinding
+import com.google.android.material.datepicker.MaterialDatePicker
 
 
 class EditProfileFragment : Fragment(R.layout.fragment_edit_profile) {
+
+    private lateinit var binding: FragmentEditProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +24,24 @@ class EditProfileFragment : Fragment(R.layout.fragment_edit_profile) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setListeners()
+
+    }
+
+    private fun setListeners() {
+
+        binding.datePickerEditProfileFragmentBirthDate.setOnClickListener {
+            val datePicker =
+                MaterialDatePicker.Builder.datePicker()
+                    .setTitleText("Fecha de nacimiento")
+                    .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                    .build()
+
+            datePicker.addOnPositiveButtonClickListener {
+                binding.datePickerEditProfileFragmentBirthDate.setText(datePicker.headerText)
+            }
+        }
 
     }
 
