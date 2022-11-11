332bf9afd136ebe4fd3db063efc581b111b6f50f
Diego <diegoguatedb2002@gmail.com>
2022-11-11 13:02:25 -0600

    Fecha parseada al obtener datos de usuario

--- Server/accessLayer/User.js
+++ Server/accessLayer/User.js
@@ -8,6 +8,14 @@ const { ContactSchema } = require("../models/contact.model");
 const { escapeRegExp } = require("../helpers/usefulFunctions");
 
 class User {
+
+	static parseUserObject(object){
+		const parsedObject = parseMongoObject(object);
+		delete parsedObject.passwordHash;
+		parsedObject.birthDate = parseDate(parsedObject.birthDate); //parse Date
+		return parsedObject
+	}
+
 	static async createUser({ name, lastName, email, birthDate, alias, passwordHash, imageUrl }) {
 		const user = new UserSchema();
 
@@ -20,17 +28,14 @@ class User {
 		user.imageUrl = imageUrl || null;
 
 		const saved = await user.save();
-		const parsedObject = parseMongoObject(saved);
-		delete parsedObject.passwordHash;
-		return parsedObject;
+		
+		return User.parseUserObject(saved);
 	}
 
 	static async updateUser(userId, newData) {
 		const updated = await UserSchema.findByIdAndUpdate(userId, newData, { new: true });
 		if (!updated) return null;
-		const parsedObject = parseMongoObject(updated);
-		delete parsedObject.passwordHash;
-		return parsedObject;
+		return User.parseUserObject(updated)
 	}
 
 	static async deleteUser(userId) {
@@ -63,11 +68,7 @@ class User {
 		});
 
 		if (!result) return null;
-		else {
-			const objParsed = parseMongoObject(result);
-			delete objParsed.passwordHash;
-			return objParsed;
-		}
+		else return User.parseUserObject(result)
 	}
 
 	constructor(userId) {
@@ -79,13 +80,7 @@ class User {
 			const data = await UserSchema.findById(this.id);
 
 			if (data === null) return null;
-
-			const parsedData = parseMongoObject(data);
-			delete parsedData.passwordHash;
-
-			//parse date
-			parsedData.birthDate = parseDate(parsedData.birthDate);
-			this.data = parsedData;
+			this.data = User.parseUserObject(data)
 		}
 
 		return this.data;
